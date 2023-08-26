99ce78fa090b723a72486f88058a85d4cf8d5ce8
pkg415 <115843385+pkg415@users.noreply.github.com>
2023-08-26 12:35:59 -0700

    some calculations for the bits

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ btn.addEventListener('click', function() {
     btn.textContent = "Share a TidBit";
   }
 })
+
+let votesInteresting = 23;
+let votesMindblowing = 23;
+let votesFalse = 77;
+const text = "Lisbon is the capital of Portugal";
+
+const strTemplateLiteral = `The current fact is ${text}. It is ${calcTidBitAge()} years old.  It is probably ${
+  totalUpvotes > votesFalse ? "correct" : "false"
+}.`
+
+let totalUpvotes = votesInteresting + votesMindblowing;
+
+const isCorrect = votesFalse < totalUpvotes;
+
+function calcTidBitAge(yearCreated) {
+  const currentYear = new Date().getFullYear();
+  if (yearCreated > currentYear) alert('no time Travel to future allowed!');
+  const age = currentYear - yearCreated;
+  return age;
+}
+
+const message = 
+  totalUpvotes > votesFalse 
+  ? "ppl vote yes" 
+  : "check more sources...";
+
+  // alert(message);
+
+  // const calcAge2 = (yearGiven) => new Date().getFullYear() - yearGiven;
+// const testAge = calcTidBitAge(2019); 
+// // console.log(testAge);
+
+// if (votesInteresting === votesMindblowing) {
+//   alert('This tidbit is equally interesting & mind-blowing!');
+// }
